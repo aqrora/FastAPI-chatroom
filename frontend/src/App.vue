960360059d19cbb606f08638d245e5adabd74685
aqrora <nicalb1441@protonmail.com>
2023-05-09 02:13:04 +0300

    message component added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,15 @@
-<script setup>
-</script>
 
 <template>
-  <div class="bg-red-500">Tailwind added</div>
+  <div class="bg-red-500">Hehe cats</div>
+  <Message/>
+  
 </template>
 
+
+
+
+
+
+<script setup>
+import Message from "./components/Message.vue"
+</script>
---
+++ frontend/src/components/Message.vue
@@ -0,0 +1,37 @@
+
+<template>
+    <div class="message-container">
+        
+        <div class="message-row">  
+            <div class="message-author">
+                Author's username
+                <!-- Author -->
+            </div>
+        </div>
+
+        <div class="message-row">
+            <div class="avatar-container">
+                <div class="avatar-container">
+                    Avatar picture
+                    <!-- Picture -->
+                </div>
+            </div>
+            <div class="message-text-container">
+                <div class="message-text">
+                    Message text
+                    <!-- Message text -->
+              
+                    <span class="sent">
+                        Date  
+                        <!-- Date -->
+                    </span>
+              
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
