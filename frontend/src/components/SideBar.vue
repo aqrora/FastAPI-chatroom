35f005866a51239977cddf64a32ead1940c3704c
aqrora <nicalb1441@protonmail.com>
2023-05-09 15:41:06 +0300

    sidebar design changes

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="border-r border-gray-300 lg:col-span-1">
     <div class="mx-3 my-3">  
-    </div>
+  </div>
 
     <ul class="overflow-auto h-[32rem]">
-      <h2 class="my-2 mb-2 ml-2 text-lg text-gray-600">Chats</h2>
+      <div class="flex items-center border-b border-gray-300">
+        <h2 class="mb-2 ml-2 text-lg text-gray-600">Chats:</h2>
+      </div>
         <li>
           <SidebarItem title="Hehe test" picture="https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg"></SidebarItem>
         </li>
