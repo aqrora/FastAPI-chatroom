58db55326a95f4c21fd955f3f9bc131151f75ef6
aqrora <nicalb1441@protonmail.com>
2023-05-19 16:43:12 +0300

    LoginPrompt added

--- frontend/src/components/LoginPrompt.vue
+++ frontend/src/components/LoginPrompt.vue
@@ -1,9 +1,34 @@
 <template>
+    <div id="loginPrompt">
+        <div class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 backdrop-filter backdrop-blur-md z-50">
+            <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-600 rounded-lg p-10 z-50 shadow-lg w-96">
+            <div class="flex w-auto justify-center">
+                <p class="mb-5 font-extrabold text-xl">Sign in</p>
+            </div>
+            <div class="flex flex-col mb-4">
+                <input class="border-none rounded-md bg-gray-200 py-3 px-4 text-lg mb-2" type="text" maxlength="32" placeholder="Username">
+                <input class="border-none rounded-md bg-gray-200 py-3 px-4 text-lg mb-2" type="text" maxlength="32" placeholder="Password">
+                <button class="my-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-4 rounded">Login</button>
+            </div>
+            <div class="flex w-auto justify-center">
 
-
+                <p>Don't have an account? <a class="underline font-bold" href="#register" onclick="switchForm()">Register one</a></p>  
+            </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 
 <script>
-export default {}
+export default {
+    methods: {
+        switchForm() {
+            // Todo
+        }
+    }
+
+
+
+}
 </script>
